7db0d366e6fffc5bb2d2950b1f33ce5f201e711c
Paiker Abbas <paikerabbas@gmail.com>
2021-11-24 22:39:24 +0300

    code updated to use custom provider

--- src/main/java/com/saien/springboottwo/HelloController.java
+++ src/main/java/com/saien/springboottwo/HelloController.java
@@ -1,7 +1,6 @@
 package com.saien.springboottwo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,19 +13,19 @@ public class HelloController {
 	@Autowired
 	private JdbcUserDetailsManager userDetailsManager;
 	
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
 
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello Paiker!!";
+		return "Hello Paiker!!@@@@@@@@@@";
 	}
 
 	@PostMapping("/user")
 	public void addUser(@RequestBody UserRequest userRequest) {
 		System.out.println("userRequestuserRequestuserRequestuserRequest");
 		System.out.println(userRequest);
-		userRequest.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+//		userRequest.setPassword(passwordEncoder.encode(userRequest.getPassword()));
 		userDetailsManager.createUser(new SecureUser(userRequest));
 	}
 
--- src/main/java/com/saien/springboottwo/ProjectBeanConfiguration.java
+++ src/main/java/com/saien/springboottwo/ProjectBeanConfiguration.java
@@ -2,13 +2,19 @@ package com.saien.springboottwo;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
@@ -16,16 +22,25 @@ public class ProjectBeanConfiguration extends WebSecurityConfigurerAdapter{
 	// we have extends this class by WebSecurityConfigurerAdapter to disable CSRF
 
 	
-    @Bean
-    public JdbcUserDetailsManager userDetailsService(){
-        return new JdbcUserDetailsManager(dataSource());
+    @Autowired
+    private MyCustomAuthenticationProvider myCustomAuthenticationProvider;
+
+   // ...
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(myCustomAuthenticationProvider);
     }
+    
+//    @Bean
+//    public JdbcUserDetailsManager userDetailsService(){
+//        return new JdbcUserDetailsManager(dataSource());
+//    }
 	
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-    	return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//    	return new BCryptPasswordEncoder();
+//    }
     
 	@Bean
 	public DataSource dataSource() {
@@ -36,7 +51,29 @@ public class ProjectBeanConfiguration extends WebSecurityConfigurerAdapter{
         return dataSource;
 	}
 	
+	@Bean
+	public JdbcUserDetailsManager jdbcUserDetailsManager() {
+		return new JdbcUserDetailsManager(dataSource());
+	}
+
 	
+    @Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails dummyUser = User.withUsername("dummy_user")
+                                    .password("1234")
+                                    .authorities("read")
+                                    .build();
+        inMemoryUserDetailsManager.createUser(dummyUser);
+        return inMemoryUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+    
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
